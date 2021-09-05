f20e37945a51a527d0772c2213883b91acdfd78e
Joa Riski <joa.riski@riskidev.fi>
2021-09-05 10:58:51 +0300

    Make the example app runnable
    Make the example app runnable to ease development

--- example/settings.py
+++ example/settings.py
@@ -4,12 +4,18 @@ import sys
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, ROOT_PATH + "/examples/")
 
+DEBUG = os.environ.get("DJANGO_DEBUG", False) in ("True", "true", "1")
 SECRET_KEY = 1
 
 INSTALLED_APPS = [
+    "django.contrib.staticfiles",
     "graphene_django",
     "example",
 ]
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+STATIC_URL = "/static/"
 
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "db.sqlite3"}}
 
@@ -21,5 +27,5 @@ TEMPLATES = [
     }
 ]
 
-GRAPHENE = {"SCHEMA": "example.schema"}
+GRAPHENE = {"SCHEMA": "example.schema.schema"}
 ROOT_URLCONF = "example.urls"
