cfab304978fd52de95ddd1adf95dee18cf26640e
Joa Riski <joa.riski@riskidev.fi>
2021-09-05 16:30:10 +0300

    Move DjangoAutoConnectionField to its own module
    Move the DjangoAutoConnectionField to a the simple_graphql.django.fields
    module, which should be better suited for it.

--- simple_graphql/django/schema/query.py
+++ simple_graphql/django/schema/query.py
@@ -1,43 +1,13 @@
-from typing import Dict, List, Optional, Type, Union
+from typing import Dict, Optional, Type
 
 import graphene
-from django.db.models import QuerySet
 from graphene import relay
-from graphene.types.mountedtype import MountedType
-from graphene.types.unmountedtype import UnmountedType
 from graphene.utils.str_converters import to_snake_case
 from graphene_django import DjangoObjectType
-from graphene_django.filter import DjangoFilterConnectionField
 
+from simple_graphql.django.fields import DjangoAutoConnectionField
 from simple_graphql.django.schema.models import ModelSchemaConfig
-from simple_graphql.django.types import ModelClass, ModelInstance
-
-
-# TODO: Move to a better location
-class DjangoAutoConnectionField(DjangoFilterConnectionField):
-    search_fields: Optional[List[str]]
-
-    def __init__(
-        self,
-        node_cls: Type[graphene.ObjectType],
-        search_fields: Optional[List[str]],
-        ordering_options: Optional[graphene.Enum],
-        **kwargs: Union[UnmountedType, MountedType],
-    ):
-        self.search_fields = search_fields
-
-        if ordering_options:
-            kwargs.setdefault("order_by", graphene.Argument(ordering_options))
-        if search_fields:
-            kwargs.setdefault("search_query", graphene.String())
-
-        super().__init__(node_cls, **kwargs)
-
-    @classmethod
-    def resolve_queryset(cls, *args, **kwargs) -> QuerySet[ModelInstance]:
-        # TODO: Implement search
-        # TODO: Implement ordering
-        return super().resolve_queryset(*args, **kwargs)
+from simple_graphql.django.types import ModelClass
 
 
 def build_ordering_enum(
